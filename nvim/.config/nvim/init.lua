2f11eeef4c0257a00448bdee65128c762d09ce8e
Matías Cañete @ Torre MSI <matiascaniete@gmail.com>
2022-12-25 04:45:21 +0100

    feat: toggle term nvim pluggin installed

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -62,6 +62,41 @@ require('packer').startup(function(use)
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
+  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup({
+      size = 20,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true, -- hide the number column in toggleterm buffers
+      shade_filetypes = {},
+      autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+      shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+      start_in_insert = true,
+      insert_mappings = true, -- whether or not the open mapping applies in insert mode
+      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+      persist_size = true,
+      persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+      direction = 'float',
+      close_on_exit = true, -- close the terminal window when the process exits
+      shell = vim.o.shell, -- change the default shell
+      auto_scroll = true, -- automatically scroll to the bottom on terminal output
+      -- This field is only relevant if direction is set to 'float'
+      float_opts = {
+        -- The border key is *almost* the same as 'nvim_open_win'
+        -- see :h nvim_open_win for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        border = 'single', -- like `size`, width and height can be a number or function which is passed the current terminal
+        winblend = 3,
+      },
+      winbar = {
+        enabled = true,
+        name_formatter = function(term) --  term: Terminal
+          return term.name
+        end
+      },
+    })
+  end }
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
@@ -123,6 +158,27 @@ vim.wo.signcolumn = 'yes'
 -- Set colorscheme
 vim.o.termguicolors = true
 vim.cmd [[colorscheme onedark]]
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazyGit = Terminal:new({
+  cmd = 'lazygit',
+  float_opts = {
+    border = "rounded",
+  },
+  hidden = true
+})
+
+vim.api.nvim_buf_create_user_command(0, 'LazyGit', function(_)
+  lazyGit:toggle()
+  print 'lg toggled!'
+end, { desc = 'LazyGit toggled!' })
+
+function _lazygit_toggle()
+  lazyGit:toggle()
+end
+
+vim.keymap.set({ 'n', 't' }, '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -184,6 +184,12 @@ _G.packer_plugins = {
     path = "/home/matias/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["toggleterm.nvim"] = {
+    config = { "\27LJ\2\2\20\0\1\2\0\1\0\0029\1\0\0L\1\2\0\tname–\3\1\0\4\0\14\0\0196\0\0\0'\1\1\0B\0\2\0029\0\2\0005\1\3\0004\2\0\0=\2\4\0016\2\5\0009\2\6\0029\2\a\2=\2\a\0015\2\b\0=\2\t\0015\2\n\0003\3\v\0=\3\f\2=\2\r\1B\0\2\1K\0\1\0\vwinbar\19name_formatter\0\1\0\1\fenabled\2\15float_opts\1\0\2\rwinblend\3\3\vborder\vsingle\nshell\6o\bvim\20shade_filetypes\1\0\r\17persist_size\2\18close_on_exit\2\14direction\nfloat\16auto_scroll\2\22terminal_mappings\2\20start_in_insert\2\17persist_mode\2\20insert_mappings\2\tsize\3\20\20shade_terminals\2\17open_mapping\n<c-\\>\14autochdir\1\17hide_numbers\2\nsetup\15toggleterm\frequire\0" },
+    loaded = true,
+    path = "/home/matias/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
+  },
   ["vim-fugitive"] = {
     loaded = true,
     path = "/home/matias/.local/share/nvim/site/pack/packer/start/vim-fugitive",
@@ -202,6 +208,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: toggleterm.nvim
+time([[Config for toggleterm.nvim]], true)
+try_loadstring("\27LJ\2\2\20\0\1\2\0\1\0\0029\1\0\0L\1\2\0\tname–\3\1\0\4\0\14\0\0196\0\0\0'\1\1\0B\0\2\0029\0\2\0005\1\3\0004\2\0\0=\2\4\0016\2\5\0009\2\6\0029\2\a\2=\2\a\0015\2\b\0=\2\t\0015\2\n\0003\3\v\0=\3\f\2=\2\r\1B\0\2\1K\0\1\0\vwinbar\19name_formatter\0\1\0\1\fenabled\2\15float_opts\1\0\2\rwinblend\3\3\vborder\vsingle\nshell\6o\bvim\20shade_filetypes\1\0\r\17persist_size\2\18close_on_exit\2\14direction\nfloat\16auto_scroll\2\22terminal_mappings\2\20start_in_insert\2\17persist_mode\2\20insert_mappings\2\tsize\3\20\20shade_terminals\2\17open_mapping\n<c-\\>\14autochdir\1\17hide_numbers\2\nsetup\15toggleterm\frequire\0", "config", "toggleterm.nvim")
+time([[Config for toggleterm.nvim]], false)
 -- Conditional loads
 time([[Conditional loading of telescope-fzf-native.nvim]], true)
   require("packer.load")({"telescope-fzf-native.nvim"}, {}, _G.packer_plugins)
