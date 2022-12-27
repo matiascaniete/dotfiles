97c801d6e3793435bef5216864943ee7ceb335f7
Matías Cañete @ Torre MSI <matiascaniete@gmail.com>
2022-12-28 00:56:20 +0100

    fix: some fixes and new pluggins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -31,6 +31,15 @@ require('packer').startup(function(use)
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   }
 
+  use { "windwp/nvim-autopairs" }
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+  }
+
   use { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     run = function()
@@ -63,40 +72,7 @@ require('packer').startup(function(use)
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
-  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
-    require("toggleterm").setup({
-      size = 20,
-      open_mapping = [[<c-\>]],
-      hide_numbers = true, -- hide the number column in toggleterm buffers
-      shade_filetypes = {},
-      autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
-      shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
-      start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-      persist_size = true,
-      persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-      direction = 'float',
-      close_on_exit = true, -- close the terminal window when the process exits
-      shell = vim.o.shell, -- change the default shell
-      auto_scroll = true, -- automatically scroll to the bottom on terminal output
-      -- This field is only relevant if direction is set to 'float'
-      float_opts = {
-        -- The border key is *almost* the same as 'nvim_open_win'
-        -- see :h nvim_open_win for details on borders however
-        -- the 'curved' border is a custom border type
-        -- not natively supported but implemented in this plugin.
-        border = 'single', -- like `size`, width and height can be a number or function which is passed the current terminal
-        winblend = 3,
-      },
-      winbar = {
-        enabled = true,
-        name_formatter = function(term) --  term: Terminal
-          return term.name
-        end
-      },
-    })
-  end }
+  use { "akinsho/toggleterm.nvim", tag = '*' }
 
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
@@ -163,6 +139,39 @@ vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_better_performance = 1
 vim.cmd [[colorscheme gruvbox-material]]
 
+require("toggleterm").setup({
+  size = 20,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  shade_filetypes = {},
+  autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+  shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+  start_in_insert = true,
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  persist_size = true,
+  persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+  direction = 'float',
+  close_on_exit = true, -- close the terminal window when the process exits
+  shell = vim.o.shell, -- change the default shell
+  auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  -- This field is only relevant if direction is set to 'float'
+  float_opts = {
+    -- The border key is *almost* the same as 'nvim_open_win'
+    -- see :h nvim_open_win for details on borders however
+    -- the 'curved' border is a custom border type
+    -- not natively supported but implemented in this plugin.
+    border = 'single', -- like `size`, width and height can be a number or function which is passed the current terminal
+    winblend = 3,
+  },
+  winbar = {
+    enabled = true,
+    name_formatter = function(term) --  term: Terminal
+      return term.name
+    end
+  },
+})
+
 local Terminal = require('toggleterm.terminal').Terminal
 local lazyGit = Terminal:new({
   cmd = 'lazygit',
@@ -172,7 +181,7 @@ local lazyGit = Terminal:new({
   hidden = true
 })
 
-vim.api.nvim_buf_create_user_command(0, 'LazyGit', function(_)
+vim.api.nvim_create_user_command('LazyGit', function(_)
   lazyGit:toggle()
   print 'lg toggled!'
 end, { desc = 'LazyGit toggled!' })
@@ -202,6 +211,7 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<leader>ff', '<cmd>NvimTreeToggle<CR>', { noremap = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -224,6 +234,8 @@ require('lualine').setup {
     section_separators = '',
   },
 }
+
+require("nvim-tree").setup()
 
 -- Enable Comment.nvim
 require('Comment').setup()
