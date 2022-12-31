a96e9ad8105efc9636632c1c76078b8f6c1273dd
Matías Cañete @ Torre MSI <matiascaniete@gmail.com>
2022-12-31 04:45:15 +0100

    feat(config): tabs config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -227,6 +227,10 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
 -- Decrease update time
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
