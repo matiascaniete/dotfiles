c659c67081e1bbbc13e76c6d2b0a34718883d39d
Matías Cañete <matiascaniete@gmail.com>
2023-01-01 16:46:39 +0100

    fix: tabs config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -230,6 +230,8 @@ vim.o.smartcase = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.smarttab = true
 
 -- Decrease update time
 vim.o.updatetime = 250
