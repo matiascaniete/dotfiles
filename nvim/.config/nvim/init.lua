028d68a2e81f2ea0296d63188c028d2352c17bf9
Matías Cañete <matiascaniete@gmail.com>
2023-02-17 18:41:21 +0100

    feat(nvim): search grep with args keymap sga

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -424,7 +424,7 @@ local on_attach = function(_, bufnr)
     nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
+    nmap("<leader>sga", require('telescope').extensions.live_grep_args.live_grep_args, "[S]earch [G]rep [A]rgs")
     -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('KK', vim.lsp.buf.signature_help, 'Signature Documentation')
