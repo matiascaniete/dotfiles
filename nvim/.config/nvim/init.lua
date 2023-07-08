a55421082609929163933c0882a1f0ed188d6e8a
Matías Cañete <matiascaniete@gmail.com>
2023-07-08 22:03:51 +0200

    install codeium pluggin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -193,6 +193,7 @@ require("packer").startup(function(use)
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
     use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 })
     use({ "akinsho/toggleterm.nvim", tag = "*" })
+    use("Exafunction/codeium.vim")
 
     -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
     local has_plugins, plugins = pcall(require, "custom.plugins")
