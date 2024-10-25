060e9f4ae37bcbd93bc01c2b3b6adda09a3e608b
Matías Cañete <matiascaniete@gmail.com>
2024-10-26 01:51:50 +0200

    fix(nvim): esc terminal mode with jk

--- configs/nvchad-2.5/.config/nvim/lua/mappings.lua
+++ configs/nvchad-2.5/.config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("t", "jk", "<C-\\><C-n>")
 map("i", "jj", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
