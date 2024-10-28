5a4144b25afbed152967ddca1f833e3da4895232
Matías Cañete <matiascaniete@gmail.com>
2024-10-28 12:30:32 +0100

    fix(nvim): ºº for exit termnal mode

--- configs/nvchad-2.5/.config/nvim/lua/mappings.lua
+++ configs/nvchad-2.5/.config/nvim/lua/mappings.lua
@@ -6,7 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("t", "jk", "<C-\\><C-n>")
+map("t", "ºº", "<C-\\><C-n>")
 map("i", "jj", "<ESC>")
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
