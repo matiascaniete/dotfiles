93c7ff02a163ad6121a5fbcb75057d1c8f1c9be0
Matías Cañete <matiascaniete@gmail.com>
2025-07-09 02:15:53 +0200

    feat(neovide): config for neovide

--- configs/nvchad-2.5/.config/nvim/lua/options.lua
+++ configs/nvchad-2.5/.config/nvim/lua/options.lua
@@ -179,3 +179,17 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.expandtab = true
 	end,
 })
+
+if vim.g.neovide then
+	vim.print("Neovide version:" .. vim.g.neovide_version)
+	vim.g.neovide_input_use_logo = true -- this will make cmd+tab work
+	vim.g.neovide_cursor_vfx_mode = "torpedo"
+	vim.g.neovide_cursor_vfx_opacity = 200
+	vim.g.neovide_scale_factor = 0.9
+	local padding = 0
+	vim.g.neovide_padding_bottom = padding
+	vim.g.neovide_padding_top = padding
+	vim.g.neovide_padding_right = padding
+	vim.g.neovide_padding_left = padding
+	vim.g.neovide_transparency = 1
+end
