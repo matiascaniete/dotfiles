eef8163cd08f61314e86cd3308c99f00493fe027
MatiasCañete <matiascaniete@gmail.com>
2025-07-10 12:48:05 +0200

    feat: add myrice

--- configs/nvchad-2.5/.config/nvim/lua/mappings.lua
+++ configs/nvchad-2.5/.config/nvim/lua/mappings.lua
@@ -5,9 +5,9 @@ require("nvchad.mappings")
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("t", "ºº", "<C-\\><C-n>")
-map("i", "jj", "<ESC>")
+map("t", "ºº", "<C-\\><C-n>", { desc = "Switch to normal mode in terminal" })
+map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+map("i", "jj", "<ESC>", { desc = "Exit insert mode more quickly" })
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "<leader>tk", "<cmd>Telescope keymaps<cr>")
