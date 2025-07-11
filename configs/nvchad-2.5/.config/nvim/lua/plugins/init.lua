632fcf189eda5cd3256f002bde070039b31cb582
Matías Cañete <matiascaniete@gmail.com>
2025-07-11 03:07:52 +0200

    feat(nvim): text object config update

--- configs/nvchad-2.5/.config/nvim/lua/plugins/init.lua
+++ configs/nvchad-2.5/.config/nvim/lua/plugins/init.lua
@@ -212,28 +212,10 @@ return {
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
-					node_incremental = "<Leader>si",
-					scope_incremental = "<Leader><Leader>",
-					node_decremental = "<Leader>sd",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					-- Automatically jump forward to textobj, similar to targets
-					lookahead = true,
-					keymaps = {
-						-- Built-in captures.
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["al"] = "@loop.outer",
-						["il"] = "@loop.inner",
-						["ac"] = "@conditional.outer",
-						["ic"] = "@conditional.inner",
-						["ihs"] = "@assignment.lhs",
-						["ahs"] = "@assignment.rhs",
-					},
+					init_selection = "<C-space>", -- set to `false` to disable one of the mappings
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<backspace>",
 				},
 			},
 		},
@@ -242,6 +224,63 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		event = "BufRead",
-		dependencies = "nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						-- Automatically jump forward to textobj, similar to targets
+						lookahead = true,
+						keymaps = {
+							-- Built-in captures.
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+
+							["am"] = "@function.outer",
+							["im"] = "@function.inner",
+
+							["af"] = "@call.outer",
+							["if"] = "@call.inner",
+
+							["al"] = "@loop.outer",
+							["il"] = "@loop.inner",
+
+							["ai"] = "@conditional.outer",
+							["ii"] = "@conditional.inner",
+
+							["a="] = "@assignment.outer",
+							["i="] = "@assignment.inner",
+							["l="] = "@assignment.lhs",
+							["r="] = "@assignment.rhs",
+
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>na"] = "@parameter.inner",
+							["<leader>nm"] = "@function.inner",
+						},
+						swap_previous = {
+							["<leader>pa"] = "@parameter.inner",
+							["<leader>pm"] = "@function.inner",
+						},
+					},
+					move = {
+						enable = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]c"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[c"] = "@class.outer",
+						},
+					},
+				},
+			})
+		end,
 	},
 }
